279fd14c1a4da792632c613c3a8fc3af13c5bf3d
Sai Vishnu <saivishnu725@gmail.com>
2025-01-01 14:45:26 +0530

    basic working for 30 items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// fizz buzz
+
 fn main() {
-    println!("Hello, world!");
+    // 3 and 5
+    for i in 1..31 {
+        let mut output = String::from("");
+        if i % 3 == 0 {
+            output.push_str("Fizz");
+        }
+        if i % 5 == 0 {
+            output.push_str("Buzz");
+        }
+        if output == "" {
+            output.push_str(&i.to_string());
+        }
+        println!("{output}");
+    }
 }
